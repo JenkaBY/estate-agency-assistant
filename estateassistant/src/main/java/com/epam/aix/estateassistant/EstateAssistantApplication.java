2941da47b88a95246cc030027aae2b0122eeed2c
YauhenKuzmich <yauhen_kuzmich@epam.com>
2025-11-24 17:59:45 +0200

    feat: init spring project

---
+++ estateassistant/src/main/java/com/epam/aix/estateassistant/EstateAssistantApplication.java
@@ -0,0 +1,13 @@
+package com.epam.aix.estateassistant;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EstateAssistantApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EstateAssistantApplication.class, args);
+    }
+
+}
---
+++ estateassistant/src/test/java/com/epam/aix/estateassistant/EstateAssistantApplicationTests.java
@@ -0,0 +1,13 @@
+package com.epam.aix.estateassistant;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EstateAssistantApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
